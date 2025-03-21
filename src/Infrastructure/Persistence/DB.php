7dff713d78544a7d69f27b6614c2a8e41be0e0ff
sanaz <sanaz.safaei@digikala.com>
2025-03-21 14:17:54 +0330

    feat: add DB manager class

---
+++ src/Infrastructure/Persistence/DB.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Infrastructure\Persistence;
+
+use PDO;
+
+class DB
+{
+    private string $path;
+    private PDO $connection;
+
+    public function __construct()
+    {
+        $this->path = 'sqlite:/'.__DIR__.'/../../../var/database.sqlite';
+        $this->connection = new PDO($this->path);
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->connection;
+    }
+}
