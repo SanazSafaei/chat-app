db08c6dedfde74846a22535b14c94652afb6e0b5
sanaz <sanaz.safaei@digikala.com>
2025-03-22 14:07:55 +0330

    feat: add fake db for persistence tests

---
+++ tests/Infrastructure/Persistence/FakeDB.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Infrastructure\Persistence;
+
+use App\Infrastructure\Persistence\DBInterface;
+use PDO;
+
+class FakeDB implements DBInterface
+{
+    private PDO $connection;
+    private string $path;
+
+    public function __construct()
+    {
+        $this->path = __DIR__ . '/fake_db.sqlite';
+        $this->connection = new PDO('sqlite:/'.$this->path);
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->connection;
+    }
+
+    public function deleteDB(): void
+    {
+        unlink($this->path);
+    }
+}
