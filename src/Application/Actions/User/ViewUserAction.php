dc4de1385b5ff313cba57c67af75719da0868589
sanaz <sanaz.safaei@digikala.com>
2025-03-23 11:53:37 +0330

    feat: add authentication for view user data

--- src/Application/Actions/User/ViewUserAction.php
+++ src/Application/Actions/User/ViewUserAction.php
@@ -14,6 +14,11 @@ class ViewUserAction extends UserAction
     protected function action(): Response
     {
         $userId = (int) $this->resolveArg('id');
+
+        if ($this->getUserId() != $userId) {
+            return $this->respondWithData(['error' => 'Unauthorized'], 401);
+        }
+
         $user = $this->userRepository->findUserOfId($userId);
 
         $this->logger->info("User of id `{$userId}` was viewed.");
