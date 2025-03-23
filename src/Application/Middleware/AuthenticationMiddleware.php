b0c1b140ed49feb8ca21aafb597c8efac33cd1f4
sanaz <sanaz.safaei@digikala.com>
2025-03-24 01:15:36 +0330

    chore: clean up code

--- src/Application/Middleware/AuthenticationMiddleware.php
+++ src/Application/Middleware/AuthenticationMiddleware.php
@@ -28,13 +28,8 @@ class AuthenticationMiddleware implements Middleware
                     $decoded = JwtManager::decode($jwt);
                     $request = $request->withAttribute('token', $decoded);
                 } catch (Exception $e) {
-                    if($e->getMessage() == 'Expired token') {
-                        $response = (new Response());
-                        return $response
-                            ->withHeader('Location', 'http://' . $_SERVER['HTTP_HOST'] . '/aut/login')
-                            ->withHeader('Authorization', '')
-                            ->withHeader('set-cookie', '')
-                            ->withStatus(302);
+                    if ($e->getMessage() == 'Expired token') {
+                        return $this->getTokenExpieredResponse();
                     }
                     $response = (new Response(401));
                     $response->getBody()->write('Unauthorized');
@@ -50,6 +45,9 @@ class AuthenticationMiddleware implements Middleware
                     $decoded = JwtManager::decode($jwt);
                     $request = $request->withAttribute('token', $decoded);
                 } catch (Exception $e) {
+                    if ($e->getMessage() == 'Expired token') {
+                        return $this->getTokenExpieredResponse();
+                    }
                     $logger = new \slim\Logger();
                     $logger->log('info', $e->getMessage());
                     throw new HttpUnauthorizedException($request);
@@ -57,5 +55,15 @@ class AuthenticationMiddleware implements Middleware
             }
         }
         return $handler->handle($request);
+    }
+
+    public function getTokenExpieredResponse(): ResponseInterface|\Psr\Http\Message\MessageInterface|Response
+    {
+        $response = (new Response());
+        return $response
+            ->withHeader('Location', 'http://' . $_SERVER['HTTP_HOST'] . '/aut/login')
+            ->withHeader('Authorization', '')
+            ->withHeader('set-cookie', '')
+            ->withStatus(302);
     }
 }
