69161264e93fe61c84881f6848158a82102e0572
sanaz <sanaz.safaei@digikala.com>
2025-03-23 22:57:20 +0330

    chore: fix code style

--- src/Application/Actions/Authentication/RegisterAction.php
+++ src/Application/Actions/Authentication/RegisterAction.php
@@ -16,8 +16,8 @@ class RegisterAction extends Action
         $baseUri = $_SERVER['HTTP_HOST'];
         return $this->response
             ->withHeader('Location', 'http://' . $baseUri . '/users/' . $user->getId())
-            ->withHeader('Authorization', 'Bearer '.$token)
-            ->withHeader('set-cookie', 'token='.$token. '; HttpOnly; Path=/')
+            ->withHeader('Authorization', 'Bearer ' . $token)
+            ->withHeader('set-cookie', 'token=' . $token . '; HttpOnly; Path=/')
             ->withStatus(302);
     }
 }
