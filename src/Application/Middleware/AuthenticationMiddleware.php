5320707a083825c419e7f0b88bd0d7dca894c785
sanaz <sanaz.safaei@digikala.com>
2025-03-23 18:13:55 +0330

    fix: remove expired token validation on login and register and fix expired token block user

--- src/Application/Middleware/AuthenticationMiddleware.php
+++ src/Application/Middleware/AuthenticationMiddleware.php
@@ -15,6 +15,10 @@ class AuthenticationMiddleware implements Middleware
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+
+        if ($request->getUri()->getPath() === '/auth/login' || $request->getUri()->getPath() === '/auth/register') {
+            return $handler->handle($request);
+        }
         if ($request->hasHeader('Authorization')) {
             $token = $request->getHeader('Authorization');
             $token = explode(' ', $token[0]);
@@ -24,6 +28,14 @@ class AuthenticationMiddleware implements Middleware
                     $decoded = JwtManager::decode($jwt);
                     $request = $request->withAttribute('token', $decoded);
                 } catch (Exception $e) {
+                    if($e->getMessage() == 'Expired token') {
+                        $response = (new Response());
+                        return $response
+                            ->withHeader('Location', 'http://' . $_SERVER['HTTP_HOST'] . '/aut/login')
+                            ->withHeader('Authorization', '')
+                            ->withHeader('set-cookie', '')
+                            ->withStatus(302);
+                    }
                     $response = (new Response(401));
                     $response->getBody()->write('Unauthorized');
                     return $response;
@@ -38,6 +50,8 @@ class AuthenticationMiddleware implements Middleware
                     $decoded = JwtManager::decode($jwt);
                     $request = $request->withAttribute('token', $decoded);
                 } catch (Exception $e) {
+                    $logger = new \slim\Logger();
+                    $logger->log('info', $e->getMessage());
                     throw new HttpUnauthorizedException($request);
                 }
             }
