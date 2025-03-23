c6bde61cdef67716e427478e608d72b6efce89d8
sanaz <sanaz.safaei@digikala.com>
2025-03-23 11:51:57 +0330

    feat: add yml secret key in settings for jwt auth method

--- src/Application/Settings/Settings.php
+++ src/Application/Settings/Settings.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace App\Application\Settings;
 
+use Webmozart\Assert\Assert;
+
 class Settings implements SettingsInterface
 {
     private array $settings;
 
     public function __construct(array $settings)
     {
+        $config = self::loadConfig();
+        $settings = array_merge($settings, $config);
         $this->settings = $settings;
     }
 
@@ -19,5 +23,42 @@ class Settings implements SettingsInterface
     public function get(string $key = '')
     {
         return (empty($key)) ? $this->settings : $this->settings[$key];
+    }
+
+    private static function loadConfig(?string $mode = null)
+    {
+
+        $configDir = __DIR__ . '/../config';
+        $dirs[] = $configDir;
+
+        if($mode) {
+            $dirs[] = $configDir . '/' . $mode;
+        }
+        $config = [];
+
+        foreach ($dirs as $dir) {
+            if (is_dir($dir)) {
+                $scanned_directory = array_diff(scandir($dir), array('..', '.'));
+                foreach ($scanned_directory as $file) {
+                    $yml = file_get_contents($dir.'/'.$file);
+                    $ymlContent = self::extractYml($yml);
+                    Assert::notNull($yml, 'Please check ' . $file);
+                    $key = explode('.', $file)[0];
+                    $config[$key] = $ymlContent;
+                }
+            }
+        }
+        return $config;
+    }
+
+    public static function extractYml(false|string $yml): array
+    {
+        $lines = explode("\n", $yml);
+        $ymlContent = [];
+        foreach ($lines as $line) {
+            $words = explode(':', $line);
+            $ymlContent[$words[0]] = $words[1];
+        }
+        return $ymlContent;
     }
 }
