f89198ab3d90718a53dd4485a4d58bfc0dd691af
sanaz <sanaz.safaei@digikala.com>
2025-03-24 17:51:56 +0330

    feat: add group owner to group members

--- src/Application/Actions/Group/CreateGroupAction.php
+++ src/Application/Actions/Group/CreateGroupAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Application\Actions\Group;
 
+use App\Domain\UseCase\Group\AddGroupMember;
 use App\Domain\UseCase\Group\CreateGroup;
 use Psr\Http\Message\ResponseInterface as Response;
 
@@ -21,6 +22,15 @@ class CreateGroupAction extends GroupAction
         $data['created_by'] = $this->getUserId();
 
         $group = (new CreateGroup($data, $this->groupRepository))->execute();
+
+        (new AddGroupMember(
+            [
+                'group_id' => $group->getId(),
+                'user_id' => $this->getUserId(),
+                'role' => $this->groupMemberRepository::ROLE_ADMIN
+            ],
+            $this->groupMemberRepository
+        ))->execute();
 
         return $this->respondWithData($group);
     }
