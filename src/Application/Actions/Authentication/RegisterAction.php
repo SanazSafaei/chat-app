ed53ff861c07786b70e5a4d1ff2705c90fdd7945
sanaz <sanaz.safaei@digikala.com>
2025-03-26 00:37:11 +0330

    feat: inject repository in RegisterAction.php

--- src/Application/Actions/Authentication/RegisterAction.php
+++ src/Application/Actions/Authentication/RegisterAction.php
@@ -2,17 +2,16 @@
 
 namespace App\Application\Actions\Authentication;
 
-use App\Application\Actions\Action;
-use App\Domain\UseCase\Authentication\JwtManager;
+use App\Application\Actions\User\UserAction;
 use App\Domain\UseCase\User\CreateUser;
 use Psr\Http\Message\ResponseInterface as Response;
 
-class RegisterAction extends Action
+class RegisterAction extends UserAction
 {
     protected function action(): Response
     {
         $data = $this->getFormData();
-        list($user, $token) = (new CreateUser($data))->execute();
+        list($user, $token) = (new CreateUser($data, $this->userRepository))->execute();
         $baseUri = $_SERVER['HTTP_HOST'];
         return $this->response
             ->withHeader('Location', 'http://' . $baseUri . '/users/' . $user->getId())
